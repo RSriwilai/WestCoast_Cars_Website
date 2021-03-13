eee63ae6d14ae6998e89f65837b4ac6c65272dca
Duk Sriwilai <duk151@hotmail.com>
2021-03-13 19:01:39 +0100

    commit

--- scripts/admin.js
+++ scripts/admin.js
@@ -3,6 +3,33 @@
 const vehicleTable = document.querySelector('#vehicle-list');
 const searchButton = document.querySelector('#search-btn');
 const searchInput = document.querySelector('#search-input');
+const addVehicleButton = document.querySelector('#addNewVehicle');
+const addVehicleView = document.querySelector('#addVehicle-container')
+const tableView = document.querySelector('#tableView');
+const saveButton = document.querySelector('#save');
+const vehicleIdInput = document.querySelector('#vehicleIdInput')
+const regNoInput = document.querySelector('#regNoInput');
+const makeInput = document.querySelector('#makeInput');
+const modelInput = document.querySelector('#modelInput');
+const modelYearInput = document.querySelector('#modelYearInput');
+const mileageInput = document.querySelector('#mileageInput');
+const valueInput = document.querySelector('#valueInput');
+const addNewButton = document.querySelector('#addNewVehicle');
+
+const addVehicle = () => {
+    const vehicle = {
+        id: vehicleIdInput.value,
+        registrationNo: regNoInput.value,
+        make: makeInput.value,
+        model: modelInput.value,
+        modelYear: modelYearInput.value,
+        mileage: mileageInput.value,
+        value: valueInput.value,
+    };
+
+    vehicles.push(vehicle);
+    createTable(vehicles);
+}
 
 function createTable(vehicleList){
     vehicleTable.innerHTML = '';
@@ -29,6 +56,21 @@ searchInput.addEventListener('keydown', (e) => {
         searchVehicle();
     }
     
+});
+
+addVehicleButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    addVehicleView.classList.remove('hidden')
+    tableView.classList.add('hidden')
+});
+
+saveButton.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    addVehicle();
+
+    addVehicleView.classList.add('hidden');
+    tableView.classList.remove('hidden');
 })
 
 function createRow(car){
